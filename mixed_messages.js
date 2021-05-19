e7b9dbc8cd48997ddfa1c3a7353030bdc0642555
timfingerhut <timfingerhut@gmail.com>
2021-05-19 09:24:24 +0100

    completed functionality of mixed messages project

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,2 +1,12 @@
-let messages = ['Vor dem Spiel ist nach dem Spiel', 'Mailand oder Madrid, Hauptsache Italien', 'Wir brauchen Eier']
+// define message database containing a number of quotes
+let messages = ['Vor dem Spiel ist nach dem Spiel', 
+                'Mailand oder Madrid, Hauptsache Italien', 
+                'Wir brauchen Eier',
+                'Wir müssen jetzt die Köpfe hochkrempeln – und die Ärmel auch.',
+                'Es ist wichtig, dass man neunzig Minuten mit voller Konzentration an das nächste Spiel denkt']
 
+// select random message
+var randomMessage = messages[Math.floor(Math.random()*messages.length)];
+
+// log the output to the console
+console.log('Dein persönliches Fußballer-Zitat des Tages: '+randomMessage)
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 - Theme: famous citations by international footballers
 - Steps: 
 
-1) Store citations in array
+1) Store footballer quotes in array
 2) Randomly select one of the citations using JavaScript's Math package.
 3) Log the message to the console whenever program is run. 
 
